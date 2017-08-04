8e9984a9ff3893e347d2e46a1a831fe3ddeecaf1
ms_vague <haha.face@gmail.com>
2017-08-04 11:42:31 -0500

    map is showing name of location and what kind of bathroom

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 $.getJSON('mock-data.json', function(data) {
 
-  let map = new google.maps.Map(document.getElementById("map"), {
+  let map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(29.9511, -90.0715),
-    zoom: 6,
+    zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
@@ -12,15 +12,27 @@ $.getJSON('mock-data.json', function(data) {
     addressArray.push((data[i].address.coord));
   } //console.log(addressArray);
 
+  let infoWindow = new google.maps.InfoWindow();
+
   for (let i = 0; i < addressArray.length; i++) {
     let latLng = new google.maps.LatLng(addressArray[i].lat, addressArray[i].lng);
 
     let marker = new google.maps.Marker({
       position: latLng,
       map: map,
-      title: data[i].name
+      name: data[i].name,
+      type: data[i].type
     });
+
+    // closure
+    (function(marker) {
+      google.maps.event.addListener(marker, 'click', function(event) {
+        infoWindow.setContent(`Bathroom at ${marker.name} is ${marker.type}.`)
+        infoWindow.open(map, marker);
+      });
+    })(marker);
   }
+
 });
 
 
