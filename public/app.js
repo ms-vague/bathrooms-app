76c38c6cfd4159c002c673a13f3bec9d078cfe28
ms_vague <haha.face@gmail.com>
2017-08-08 16:27:42 -0500

    fetching data from GET route

--- public/app.js
+++ public/app.js
@@ -1,38 +1,68 @@
-$.getJSON('mock-data.json', function(data) {
+var RESULTS_URL = '/bathrooms';
 
+let stateFacet = (function() {
+
+  let state = {};  
+
+  function setData(bathrooms) {
+    state.bathrooms = bathrooms;
+    //console.log(state);
+  }
+
+  return {
+     setData: setData
+  }
+
+}());
+
+
+$.getJSON(RESULTS_URL, function(data) {
+  //console.log(data);
+ 
+  stateFacet.setData(data);
+  // antiquated. redo in jQuery -- with init function
+
+  // get map to show up in browser
   let map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(29.9511, -90.0715),
     zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  let addressArray = [];
+  for (let bathrooms in data);
+  let bathroomsArray = data.bathrooms;
+  //console.log(bathroomsArray);
 
-  for (let i = 0; i < data.length; i++) {
-    addressArray.push((data[i].address.coord));
-  } //console.log(addressArray);
+  let locationsArray = [];
+
+  for (let i = 0; i < bathroomsArray.length; i++) {
+    locationsArray.push(bathroomsArray[i].coords);
+  }
+  //console.log(locationsArray);
 
   let infoWindow = new google.maps.InfoWindow();
 
-  for (let i = 0; i < addressArray.length; i++) {
-    let latLng = new google.maps.LatLng(addressArray[i].lat, addressArray[i].lng);
+  // loop thru address array, grap lat and lngcoords from address object
+  for (let i = 0; i < locationsArray.length; i++) {
+    //console.log(locationsArray[i]);
+    let latLng = new google.maps.LatLng(locationsArray[i].lat, locationsArray[i].lng);
 
+    // build marker object, grab name and type from data json 
     let marker = new google.maps.Marker({
       position: latLng,
       map: map,
-      name: data[i].name,
-      type: data[i].type
+      name: bathroomsArray[i].name,
+      type: bathroomsArray[i].type
     });
 
-    // closure
+    // use closure to make infowindow appear on click
     (function(marker) {
       google.maps.event.addListener(marker, 'click', function(event) {
-        infoWindow.setContent(`Bathroom at ${marker.name} is ${marker.type}.`)
+        infoWindow.setContent(`Bathroom at ${marker.name} is ${marker.type}.`);
         infoWindow.open(map, marker);
       });
     })(marker);
   }
-
 });
 
 
