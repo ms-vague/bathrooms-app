04fd340e636b92e030caab85ec4d78204b358357
ms_vague <haha.face@gmail.com>
2017-08-21 20:45:44 -0500

    using .getJson inside revealing module

--- public/app.js
+++ public/app.js
@@ -2,88 +2,26 @@ var RESULTS_URL = '/bathrooms';
 
 let stateFacet = (function() {
 
-  let state = {};  
+  let state = {
+  };  
 
-  //console.log('before getData', state);
+  function getBathroomsFromServer() {
+    $.getJSON(RESULTS_URL, function(bathrooms) {
+        state = bathrooms;
+    });
+  } 
+  getBathroomsFromServer();
 
-  function getData(bathrooms) {
-    //console.log('before assignment inside getData', state);
+  function getBathrooms(bathrooms) {
     state = bathrooms;
-    //console.log('inside getData', state);
   }
 
   function seeData() {
-    //console.log('inside seeData', state);
     return state;
   }
 
   return {
-     setData: getData,
-     showData: seeData
+    showData: seeData
   }
 
 }());
-
-function initMap(state) {
-  //console.log(state);
-  let coords = state.bathrooms[0].coords;
-
-  let map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(coords),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-}
-
-
-$.getJSON(RESULTS_URL, function(data) {
-  //console.log(data);
-
-  stateFacet.setData(data);
-  initMap(stateFacet.showData());
-  // antiquated. redo in jQuery -- with init function
-
-  // get map to show up in browser
-  /*let map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(29.9511, -90.0715),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-
-  for (let bathrooms in data);
-  let bathroomsArray = data.bathrooms;
-  //console.log(bathroomsArray);
-
-  let locationsArray = [];
-
-  for (let i = 0; i < bathroomsArray.length; i++) {
-    locationsArray.push(bathroomsArray[i].coords);
-  }
-  //console.log(locationsArray);
-
-  let infoWindow = new google.maps.InfoWindow();
-
-  // loop thru address array, grap lat and lngcoords from address object
-  for (let i = 0; i < locationsArray.length; i++) {
-    //console.log(locationsArray[i]);
-    let latLng = new google.maps.LatLng(locationsArray[i].lat, locationsArray[i].lng);
-
-    // build marker object, grab name and type from data json 
-    let marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-      name: bathroomsArray[i].name,
-      type: bathroomsArray[i].type
-    });
-
-    // use closure to make infowindow appear on click
-    (function(marker) {
-      google.maps.event.addListener(marker, 'click', function(event) {
-        infoWindow.setContent(`Bathroom at ${marker.name} is ${marker.type}.`);
-        infoWindow.open(map, marker);
-      });
-    })(marker);
-  }*/
-});
-
-
