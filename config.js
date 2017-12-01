0351ee30561915cda6c8af29f9004fef98c7772b
ms_vague <haha.face@gmail.com>
2017-11-30 19:27:13 -0600

    Add config.js for heroku app

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+      global.DATABASE_URL || 'mongodb://localhost/bathrooms-app'
+
+exports.TEST_DATABASE_URL = (
+  process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-bathrooms-app');
+
+exports.PORT = process.env.PORT || 8080;
+
+exports.TEST_PORT = 8081;
+
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
